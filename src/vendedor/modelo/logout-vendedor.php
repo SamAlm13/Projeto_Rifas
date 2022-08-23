8b2ce4cb422ca6106e67c35180e6ebd002699594
SamAlm13 <rubgmer1001@gmail.com>
2022-08-23 10:22:06 -0300

    commit 2308

---
+++ src/vendedor/modelo/logout-vendedor.php
@@ -0,0 +1,11 @@
+<?php
+
+    session_start();
+    session_destroy();
+
+$dados = array(
+    "tipo" => "success",
+    "mensagem" => "Sua sessão foi encerrada."
+);
+
+echo json_encode($dados);
--- src/vendedor/modelo/validate-vendedor.php
+++ src/vendedor/modelo/validate-vendedor.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-if(!isset($_SESSION['NOME']) && ($_SESSION['TIPO'])){
+if(!isset($_SESSION['NOME']) && !isset($_SESSION['TIPO'])){
     $dados = array(
         'tipo' => 'error',
         'mensagem' => 'Você não está autenticado no Sistema.'
